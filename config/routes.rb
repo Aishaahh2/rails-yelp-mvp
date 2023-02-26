c44421fa8880af9a59bb4c2db26c61c27880ec95
Aisha Hersi <aishahersi509@gmail.com>
2023-02-26 10:00:11 +0000

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # get 'restaurants/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+    end
+  end
